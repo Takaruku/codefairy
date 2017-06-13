1602942c85d0b4a30bb8d9d544676d7f57469dd7
067860072 <067860072@109R031001904RP.EDU.YRDSB.NET>
2017-06-13 13:44:45 -0400

    Merge origin/master
    Conflicts:
    	src/codefairies/User.java

---
+++ src/codefairies/Exam.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codefairies;
+
+/**
+ *
+ * @author 067860072
+ */
+public class Exam {
+     // problem solving, understand oop or linked list
+    
+    // Non Ap
+    // classes with missing information, fill in with oop info 
+    
+    // AP - Linked link question (problem solving)
+    // write a method for a double linked list 
+    // [thinking question]
+    // 6 marks
+    
+    // two programs you will hgave to write
+    // one is a super hard thinking question to save till last, take a long time
+    // has to do with project management
+    
+    // last question:
+    // read a delimited file, file is same format (music, books)
+    // manipulate it
+    // do some stuff with it:
+    // eg. find all the books by rowling
+    
+    // no gui or graphics
+    // may ask a few gui, graphics, or timer question on multiple choice
+    
+    // 4 sections
+    // 1 = multiple choice
+    //    2 = thinking and communication
+    //  3 and 4 = 2 programs to write
+}
